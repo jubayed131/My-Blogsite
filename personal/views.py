da17579090430d2486498ac43f379fd0ef5ddd7c
jubayed131 <jubayed.cste@gmail.com>
2016-11-23 21:14:31 +0100

    another

---
+++ personal/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'personal/home.html')
+def contact(request):
+    return render(request, 'personal/basic.html',{'content':['If you like to contact me,Please email me','jubayed.developer@gmail.com']})
